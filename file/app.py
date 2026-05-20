29dab6826ba9c4cf8e418d17eb47b5006385c972
Om <omughade33@gmail.com>
2026-05-20 19:44:02 +0530

    show backend error

--- file/app.py
+++ file/app.py
@@ -117,10 +117,9 @@ def register():
 
         return jsonify({'success': True, 'user': {'id': user_id, 'name': name, 'role': role}}), 201
     except Exception as e:
-        import traceback
-        traceback.print_exc()
-        print("Register error:", e)
-        return jsonify({'error': str(e)}), 500
+         return jsonify({
+        "error": str(e)
+        }), 500
 
 @app.route('/api/login', methods=['POST'])
 def login():
